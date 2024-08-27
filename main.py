55ad9babdf8326aab35c4df150c8d15bd896ccc6
JakenPring <162660319+JakenPring@users.noreply.github.com>
2024-08-27 04:36:10 +0000

    Menu start

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import time
 
 def clear():
     os.system('clear')
+
+def wait(seconds):
+    time.sleep(seconds)
 
 # CSV setup
 trips_2019_heading = "Trips Ending March 2019"
@@ -37,47 +41,117 @@ reason_df.to_csv('Reasons_For_Travel_cleaned.csv', index=True)
 new_df = pd.read_csv('Tourism_cleaned.csv')
 new_reason_df = pd.read_csv('Reasons_For_Travel_cleaned.csv')
 
-def list_countries():
-    countries = new_df['Country'].tolist()
-    for i in range(len(countries)):
-        print(f"{i+1}. {countries[i]}")
+total_holiday_2019 = new_reason_df.loc[0, trips_2019_heading]
+total_holiday_2024 = new_reason_df.loc[0, trips_2024_heading]
+total_visiting_2019 = new_reason_df.loc[1, trips_2019_heading]
+total_visiting_2024 = new_reason_df.loc[1, trips_2024_heading]
+total_business_2019 = new_reason_df.loc[2, trips_2019_heading]
+total_business_2024 = new_reason_df.loc[2, trips_2024_heading]
+total_employment_2019 = new_reason_df.loc[3, trips_2019_heading]
+total_employment_2024 = new_reason_df.loc[3, trips_2024_heading]
+total_education_2019 = new_reason_df.loc[4, trips_2019_heading]
+total_education_2024 = new_reason_df.loc[4, trips_2024_heading]
+total_other_2019 = new_reason_df.loc[5, trips_2019_heading]
+total_other_2024 = new_reason_df.loc[5, trips_2024_heading]
 
-def get_single_data(country, heading):
-    pass
+# Menu options
+def exitOption():
+    input("Press enter to return to menu. ")
 
-# Menu system
-action_loop = True
-int_action = 0
-clear()
-print("Welcome to the tourism tracker, the easiest way to discover all the tourism information you need about Australia, from 2019 and 2024.")
-def main_loop():
-    global action_loop, int_action
-    print("What would you like to do?")
-    print("1. Find the number of tourists to a country at a specific time")
-    print("2. Create a graph comparing tourists to a country from 2019 and 2024")
-    print("3. Create a graph of tourism numbers in a specific year")
-    print("4. Stop the program")
-    given_action = False
-    while not given_action:
-        action = input("What would you like to do?: ")
-        try:
-            int_action = int(action)
-            if int_action <= 0 or int_action >= 5:
-                print("Invalid action, please input an option from 1-4")
-            else:
-                given_action = True
-        except ValueError:
-            print("Please input a number.")
-    if int_action == 1:
-        print("Which country from this list would you like to see?")
-        list_countries()
-    elif int_action == 2:
-        pass
-    elif int_action == 3:
-        pass
-    elif int_action == 4:
-        action_loop = False
-while action_loop:
-    main_loop()
+def optionOne():
+    print(df)
+    exitOption()
+
+def optionTwo():
+    total_2019 = new_df[trips_2019_heading].sum()
+    total_2024 = new_df[trips_2024_heading].sum()
+    plt.figure(figsize=(8, 6))
+    plt.bar(['2019', '2024'], [total_2019, total_2024], color=['blue', 'orange'])
+    plt.xlabel('Year')
+    plt.ylabel('Total Number of Travelers')
+    plt.ylim(6000, 8750)
+    plt.title('Total Number of Travelers in 2019 vs. 2024')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.savefig('Travelers_Comparison.png')
+    plt.show()
+    print("If the graph didn't automatically open, open the Travelers_Comparison.png file to view.")
+    exitOption()
+
+def optionThreeA():
+    plt.figure(figsize=(8, 6))
+    plt.bar(['Holiday', 'Visiting', 'Business', 'Employment', 'Education', 'Other'], [total_holiday_2019, total_visiting_2019, total_business_2019, total_employment_2019, total_education_2019, total_other_2019], color=['red','green','blue','yellow','purple','orangered'])
+    plt.xlabel('Reason')
+    plt.ylabel('Total number of travelers')
+    plt.title('Travel reasons in 2019')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.savefig('Travelers_Reasons_2019.png')
+    plt.show()
+    print("If the graph didn't automatically open, open the Travelers_Reasons_2019.png file to view.")
+    exitOption()
+
+def optionThreeB():
+    plt.figure(figsize=(8, 6))
+    plt.bar(['Holiday', 'Visiting', 'Business', 'Employment', 'Education', 'Other'], [total_holiday_2024, total_visiting_2024, total_business_2024, total_employment_2024, total_education_2024, total_other_2024], color=['lightcoral','palegreen','paleturquoise','palegoldenrod','violet','lightsalmon'])
+    plt.xlabel('Reason')
+    plt.ylabel('Total number of travelers')
+    plt.title('Travel reasons in 2024')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.savefig('Travelers_Reasons_2024.png')
+    plt.show()
+    print("If the graph didn't automatically open, open the Travelers_Reasons_2024.png file to view.")
+    exitOption()
+
+def optionThreeC():
+    plt.figure(figsize=(22, 6))
+    plt.bar(['Holiday 2019', 'Holiday 2024', 'Visiting 2019', 'Visiting 2024', 'Business 2019', 'Business 2024', 'Employment 2019', 'Employment 2024', 'Education 2019', 'Education 2024', 'Other 2019', 'Other 2024'], [total_holiday_2019, total_holiday_2024, total_visiting_2019, total_visiting_2024, total_business_2019, total_business_2024, total_employment_2019, total_employment_2024, total_education_2019, total_education_2024, total_other_2019, total_other_2024], color=['red','lightcoral','green','palegreen','blue','paleturquoise','goldenrod','palegoldenrod','purple','violet','orangered','lightsalmon'])
+    plt.xlabel('Reason')
+    plt.ylabel('Total number of travelers')
+    plt.title('Travel reasons in 2019 and 2024')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.savefig('Travelers_Reasons_Comparison.png')
+    plt.show()
+    print("If the graph didn't automatically open, open the Travelers_Reasons_Comparison.png file to view.")
+    exitOption()
+
+def optionFour():
+    holiday_percent_2019 = ((total_holiday_2019/total_2019)*100).round(2)
+    holiday_percent_2024 = ((total_holiday_2024/total_2019)*100).round(2)
+    print(f"In 2019, {holiday_percent_2019}% of tourists were here for a holiday.")
+    print(f"In 2024, {holiday_percent_2024}% of tourists were here for a holiday.")
+
+# Menu System
+inMenuLoop = True
+while inMenuLoop:
     clear()
-print("Stopping the program...\n\nThank you for using the tourism tracker.")
+    print("Welcome to the Tourism Data Analysis Program")
+    print("Please select an option from the menu below")
+    print("1. Show the dataframe")
+    print("2. Create a bar chart comparing 2019 and 2024")
+    print("3. Create a bar chart of the reasons for travel")
+    print("4. Percentage of tourists for reason")
+    chosenOption = input("Select your option: ").lower()
+    if chosenOption == "1" or chosenOption == "one":
+        optionOne()
+    elif chosenOption == "2" or chosenOption == "two":
+        optionTwo()
+    elif chosenOption == "3" or chosenOption == "three":
+        option3year = input("Would you like to see 2019, 2024, or a comparison of both?").lower()
+        if option3year == "2019" or option3year == "19":
+            optionThreeA()
+        elif option3year == "2024" or option3year == "24":
+            optionThreeB()
+        elif option3year == "both":
+            optionThreeC()
+        else:
+            print("Invalid option.")
+    elif chosenOption == "4" or chosenOption == "four":
+        optionFour()
+    elif chosenOption == "exit":
+        exitConfirm = input("Are you sure you want to close the program?: ").lower()
+        if exitConfirm == "y" or exitConfirm == "yes":
+            inMenuLoop = False
+        elif exitConfirm == "n" or exitConfirm == "no":
+            exitConfirm
+    else:
+        print("Invalid Option. Please choose a number listed above.")
+print("Program closed.")
